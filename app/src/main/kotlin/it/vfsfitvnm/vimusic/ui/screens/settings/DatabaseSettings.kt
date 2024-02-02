fdb8eddcf95695e2b515740a9c4034169ba84663
Rino Russo <rinorusso@gmail.com>
2024-02-02 19:45:24 +0100

    Added confirmation dialog in backup and restore database backup

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/settings/DatabaseSettings.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/settings/DatabaseSettings.kt
@@ -5,22 +5,34 @@ import android.content.ActivityNotFoundException
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
 import it.vfsfitvnm.vimusic.Database
 import it.vfsfitvnm.vimusic.LocalPlayerAwareWindowInsets
@@ -30,9 +42,14 @@ import it.vfsfitvnm.vimusic.path
 import it.vfsfitvnm.vimusic.query
 import it.vfsfitvnm.vimusic.service.MyDownloadService
 import it.vfsfitvnm.vimusic.service.PlayerService
+import it.vfsfitvnm.vimusic.ui.components.themed.ConfirmationDialog
+import it.vfsfitvnm.vimusic.ui.components.themed.DefaultDialog
 import it.vfsfitvnm.vimusic.ui.components.themed.HeaderWithIcon
 import it.vfsfitvnm.vimusic.ui.styling.LocalAppearance
+import it.vfsfitvnm.vimusic.ui.styling.shimmer
+import it.vfsfitvnm.vimusic.utils.bold
 import it.vfsfitvnm.vimusic.utils.intent
+import it.vfsfitvnm.vimusic.utils.semiBold
 import it.vfsfitvnm.vimusic.utils.toast
 import kotlinx.coroutines.flow.distinctUntilChanged
 import java.io.FileInputStream
@@ -45,7 +62,7 @@ import kotlin.system.exitProcess
 @Composable
 fun DatabaseSettings() {
     val context = LocalContext.current
-    val (colorPalette) = LocalAppearance.current
+    val (colorPalette, typography) = LocalAppearance.current
 
     val eventsCount by remember {
         Database.eventsCount().distinctUntilChanged()
@@ -67,6 +84,8 @@ fun DatabaseSettings() {
             }
         }
 
+    var exitAfterRestore by remember { mutableStateOf(false) }
+
     val restoreLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
             if (uri == null) return@rememberLauncherForActivityResult
@@ -84,9 +103,76 @@ fun DatabaseSettings() {
                 context.stopService(context.intent<PlayerService>())
                 context.stopService(context.intent<MyDownloadService>())
 
-                exitProcess(0)
+                exitAfterRestore = true
             }
         }
+
+    if (exitAfterRestore)
+        DefaultDialog(
+            onDismiss = { exitAfterRestore = false },
+            content = {
+                BasicText(
+                    text = stringResource(R.string.restore_completed),
+                    style = typography.s.bold.copy(color = colorPalette.text),
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                BasicText(
+                    text = stringResource(R.string.click_to_close),
+                    style = typography.xs.semiBold.copy(color = colorPalette.textSecondary),
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Image(
+                    painter = painterResource(R.drawable.server),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(colorPalette.shimmer),
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clickable {
+                            exitAfterRestore = false
+                            exitProcess(0)
+                        }
+                )
+            }
+
+        )
+
+    var isExporting by remember { mutableStateOf(false) }
+    var isImporting by remember { mutableStateOf(false) }
+
+    if (isExporting) {
+        ConfirmationDialog(
+            text = stringResource(R.string.export_the_database),
+            onDismiss = { isExporting = false },
+            onConfirm = {
+                @SuppressLint("SimpleDateFormat")
+                val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
+                try {
+                    backupLauncher.launch("rimusic_${dateFormat.format(Date())}.db")
+                } catch (e: ActivityNotFoundException) {
+                    context.toast("Couldn't find an application to create documents")
+                }
+            }
+        )
+    }
+    if (isImporting) {
+        ConfirmationDialog(
+            text = stringResource(R.string.import_the_database),
+            onDismiss = { isImporting = false },
+            onConfirm = {
+                try {
+                    restoreLauncher.launch(
+                        arrayOf(
+                            "application/vnd.sqlite3",
+                            "application/x-sqlite3",
+                            "application/octet-stream"
+                        )
+                    )
+                } catch (e: ActivityNotFoundException) {
+                    context.toast("Couldn't find an application to open documents")
+                }
+            }
+        )
+    }
 
     Column(
         modifier = Modifier
@@ -131,16 +217,10 @@ fun DatabaseSettings() {
             title = stringResource(R.string.backup_1),
             text = stringResource(R.string.export_the_database),
             onClick = {
-                @SuppressLint("SimpleDateFormat")
-                val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
-
-                try {
-                    backupLauncher.launch("rimusic_${dateFormat.format(Date())}.db")
-                } catch (e: ActivityNotFoundException) {
-                    context.toast("Couldn't find an application to create documents")
-                }
+                isExporting = true
             }
         )
+
 
         SettingsGroupSpacer()
 
@@ -155,17 +235,7 @@ fun DatabaseSettings() {
             title = stringResource(R.string.restore_1),
             text = stringResource(R.string.import_the_database),
             onClick = {
-                try {
-                    restoreLauncher.launch(
-                        arrayOf(
-                            "application/vnd.sqlite3",
-                            "application/x-sqlite3",
-                            "application/octet-stream"
-                        )
-                    )
-                } catch (e: ActivityNotFoundException) {
-                    context.toast("Couldn't find an application to open documents")
-                }
+                isImporting = true
             }
         )
     }
