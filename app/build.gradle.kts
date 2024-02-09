d42a423460bf8ad02b8c3a27f14f373f2587e955
Rino Russo <rinorusso@gmail.com>
2024-02-09 23:11:02 +0100

    New release version 0.6.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.fast4x.rimusic"
         minSdk = 21
         targetSdk = 34
-        versionCode = 16
-        versionName = "0.6.20"
+        versionCode = 17
+        versionName = "0.6.21"
         buildConfigField("String", "VERSION_NAME", "\"$versionName\"")
     }
 
