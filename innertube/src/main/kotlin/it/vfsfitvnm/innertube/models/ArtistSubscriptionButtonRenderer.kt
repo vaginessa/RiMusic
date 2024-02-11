7a1b560ad8465b7222eb96a2a518cc0d80c6c302
Rino Russo <rinorusso@gmail.com>
2024-02-11 15:34:10 +0100

    Improved artist page #619

---
+++ innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/ArtistSubscriptionButtonRenderer.kt
@@ -0,0 +1,14 @@
+package it.vfsfitvnm.innertube.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SubscriptionButton(
+    val subscribeButtonRenderer: SubscribeButtonRenderer,
+) {
+    @Serializable
+    data class SubscribeButtonRenderer(
+        val subscriberCountText: Runs,
+    )
+
+}
